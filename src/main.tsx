514d4b0c8c1f38180677f61def387f1876bc2827
Vansh_Dave <vanshdave0409@gmail.com>
2026-08-29 14:24:07 +0530

    Add script tag for main.tsx in index.html

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+<!-- index.html -->
+<script type="module" src="/src/main.tsx"></script>
+  
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
